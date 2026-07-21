dac4d74dbe9f3a67f904dbad9fceb3a14d95ba70
Chamu <chamuditha.rajapaksha@gmail.com>
2026-07-21 19:47:40 +0530

    Added macro manager page

---
+++ src/components/features/macro/MacroManager.tsx
@@ -0,0 +1,9 @@
+export default function MacroManager() {
+    return (
+        <div style={{ padding: 24 }}>
+            <h1>Macro Manager</h1>
+
+            <p>No macros yet.</p>
+        </div>
+    );
+}
